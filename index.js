391161b8343c0c52d1df293ccab90cd541a0476c
bayulee <bayuleepaul@gmail.com>
2022-04-14 14:33:45 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const morgan=require("morgan")
+require("dotenv").config()
+const connectDB=require("./configDB/connectDB")
+const foodRoute=require("./routes/foodRoute")
+
+const app=express();
+
+
+connectDB()
+//middleware
+app.use(express.json())
+app. use(morgan("dev"))
+app.use(foodRoute)
+const PORT=process.env.PORT||6005
+//home route
+app.get("/",(req,res)=>{
+    res.json("WELCOME TO MY MENU API.")
+})
+
+
+app.listen(PORT, ()=>{
+console.log("SERVER IS UP!")
+})
